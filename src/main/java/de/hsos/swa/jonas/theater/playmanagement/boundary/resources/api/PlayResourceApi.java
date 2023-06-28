f56cf1df49327c32a288afd34d289dc83379ddec
Jonasn97 <47939597+Jonasn97@users.noreply.github.com>
2023-06-28 22:54:18 +0200

    chore: added openapi definitions and fault tolerance, minor optimizations

--- src/main/java/de/hsos/swa/jonas/theater/playmanagement/boundary/resources/api/PlayResourceApi.java
+++ src/main/java/de/hsos/swa/jonas/theater/playmanagement/boundary/resources/api/PlayResourceApi.java
@@ -5,6 +5,13 @@ import de.hsos.swa.jonas.theater.playmanagement.boundary.dto.QueryParametersDTO;
 import de.hsos.swa.jonas.theater.playmanagement.control.PlayOperations;
 import de.hsos.swa.jonas.theater.shared.*;
 import io.quarkus.logging.Log;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Timeout;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -29,6 +36,17 @@ public class PlayResourceApi {
 
 
     @GET
+    @Retry
+    @Timeout(5000)
+    @Fallback(fallbackMethod = "getPlaysFallback")
+    @CircuitBreaker(requestVolumeThreshold = 4, failureRatio = 0.75, delay = 10000)
+    @Operation(summary = "Get filtered Plays", description = "Get filtered and paged Plays")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Filtered Plays are returned"),
+            @APIResponse(responseCode = "400", description = "Bad Request"),
+            @APIResponse(responseCode = "404", description = "No Plays found for selected filter"),
+            @APIResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public Response getPlays(@QueryParam("filter[name]") String nameFilter,
                              @QueryParam("filter[status]") ArrayList<String> statusFilter,
                              @QueryParam("filter[playType]") ArrayList<String> playTypeFilter,
@@ -76,7 +94,21 @@ public class PlayResourceApi {
         responseWrapperDTO.links = createPaginationLinks(queryParametersDTO);
         return Response.ok().entity(responseWrapperDTO).build();
     }
-
+    public Response getPlaysFallback(@QueryParam("filter[name]") String nameFilter,
+                                     @QueryParam("filter[status]") ArrayList<String> statusFilter,
+                                     @QueryParam("filter[playType]") ArrayList<String> playTypeFilter,
+                                     @QueryParam("filter[performanceType]") ArrayList<String> performanceTypeFilter,
+                                     @QueryParam("filter[startDateTime]") String startDateTimeFilter,
+                                     @QueryParam("filter[endDateTime]") String endDateTimeFilter,
+                                     @QueryParam("include") String include,
+                                     @DefaultValue(FIRSTPAGESTRING)@QueryParam("page[number]") Long pageNumber,
+                                     @DefaultValue("10")@QueryParam("page[size]") Long pageSize) {
+        ResponseWrapperDTO responseWrapperDTO = new ResponseWrapperDTO();
+        List<ErrorDTO> errors = new ArrayList<>();
+        errors.add(new ErrorDTO("500", "PLAYS:2","Internal Server Error", "Something went wrong while processing your request"));
+        responseWrapperDTO.errors = errors;
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(responseWrapperDTO).build();
+    }
     private LinksDTO createSelfLink(String id) {
         LinksDTO linksDTO = new LinksDTO();
         linksDTO.self = uriInfo.getBaseUriBuilder()
@@ -103,15 +135,15 @@ public class PlayResourceApi {
                 .toString();
         if(pageNumber>FIRSTPAGE)
             linksDTO.prev = uriBuilder
-                    .replaceQueryParam("page[number]", pageNumber-1)
-                    .build()
+                    .replaceQueryParam("page[number]", "{pageNumber}")
+                    .build(pageNumber-1)
                     .toString();
         else
             linksDTO.prev = "";
         if((pageNumber+1) * pageSize < maxSize)
             linksDTO.next = uriBuilder
-                .replaceQueryParam("page[number]", pageNumber+1)
-                .build()
+                .replaceQueryParam("page[number]", "{pageNumber}")
+                .build(pageNumber+1)
                 .toString();
         else
             linksDTO.next = "";
@@ -124,3 +156,4 @@ public class PlayResourceApi {
 
 
 }
+//-Xlint:unchecked
