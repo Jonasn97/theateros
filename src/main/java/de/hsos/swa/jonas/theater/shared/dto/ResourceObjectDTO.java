0f3fc1f23a71cc25e3bf69367839295d0713b31a
Jonasn97 <47939597+Jonasn97@users.noreply.github.com>
2023-07-13 10:29:46 +0200

    fix: updated ResourceObjectDTO to contain Relationships

--- src/main/java/de/hsos/swa/jonas/theater/shared/dto/ResourceObjectDTO.java
+++ src/main/java/de/hsos/swa/jonas/theater/shared/dto/ResourceObjectDTO.java
@@ -4,14 +4,14 @@ public class ResourceObjectDTO<T> {
     public String id;
     public String type;
     public T attributes;
+    public RelationshipDTO<T> relationships;
     public LinksDTO links;
 
-    //relationships
-
-    public ResourceObjectDTO(String id,String type, T attributes, LinksDTO links) {
+    public ResourceObjectDTO(String id,String type, T attributes, RelationshipDTO<T> relationships, LinksDTO links) {
         this.id = id;
         this.type = type;
         this.attributes = attributes;
+        this.relationships = relationships;
         this.links = links;
     }
 
