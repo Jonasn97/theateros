5fb931365075842ca85d5b97f71132eeb072f563
Jonas Nelsn <jonas.nelson@hs-osnabrueck.de>
2023-07-18 23:19:55 +0200

    feat: added baselayout to login-screen and deins-screen, chore: changed login path and loggedIn Path in application.properties, changed getLoggedIn Page to UserResourceMobile

--- src/main/java/de/hsos/swa/jonas/theater/security/boundary/mobile/LoginResourceMobile.java
+++ src/main/java/de/hsos/swa/jonas/theater/security/boundary/mobile/LoginResourceMobile.java
@@ -11,14 +11,12 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
-@Path("")
+import java.util.Set;
+
+@Path("/mobile")
 public class LoginResourceMobile {
     @Inject
     Template login;
-    @Inject
-    Template loggedIn;
-    @Inject
-    SecurityIdentity identity;
 
     @Path("/login")
     @GET
@@ -28,13 +26,5 @@ public class LoginResourceMobile {
         return Response.ok().entity(html).build();
     }
 
-    @Path("/deins")
-    @RolesAllowed("user")
-    @GET
-    @Produces(MediaType.TEXT_HTML)
-    public Response getLoggedInPage(@Context SecurityContext securityContext){
-        TemplateInstance instance = loggedIn.data("user", identity.getPrincipal().getName());
-        String html = instance.render();
-        return Response.ok().entity(html).build();
-    }
+
 }
---
+++ src/main/java/de/hsos/swa/jonas/theater/userdata/boundary/resource/mobile/UserResourceMobile.java
@@ -0,0 +1,35 @@
+package de.hsos.swa.jonas.theater.userdata.boundary.resource.mobile;
+
+import io.quarkus.logging.Log;
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+
+import javax.annotation.security.RolesAllowed;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+
+@Path("/mobile")
+public class UserResourceMobile {
+    @Inject
+    Template yours;
+
+    @Path("/deins")
+    @RolesAllowed("user")
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    @Transactional(Transactional.TxType.REQUIRES_NEW)
+    public Response getLoggedInPage(@Context SecurityContext securityContext){
+        String username = securityContext.getUserPrincipal().getName();
+        TemplateInstance instance = yours.data("user", username);
+        String html = instance.render();
+        return Response.ok().entity(html).build();
+    }
+}
