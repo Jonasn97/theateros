8da3f8d762a2e1941251f072d0df9a2ceaf82913
Jonas Nelsn <jonas.nelson@hs-osnabrueck.de>
2023-07-19 22:42:32 +0200

    feat: added css-classes and labels for Event-State-Enum

--- src/main/java/de/hsos/swa/jonas/theater/userdata/entity/EventState.java
+++ src/main/java/de/hsos/swa/jonas/theater/userdata/entity/EventState.java
@@ -1,10 +1,36 @@
 package de.hsos.swa.jonas.theater.userdata.entity;
 
 public enum EventState {
-    INTERESTED,
-    NOT_INTERESTED,
-    UNSURE,
-    MISS_OUT,
-    SEEN,
-    NONE
+    INTERESTED("Interessiert"),
+    NOT_INTERESTED("Kein Interesse"),
+    UNSURE("Unsicher"),
+    MISS_OUT("Verpasst"),
+    SEEN("Gesehen"),
+    NONE("Nicht gesetzt");
+
+    private final String germanLabel;
+
+    EventState(String germanLabel) {
+        this.germanLabel = germanLabel;
+    }
+    public String getLabel() {
+        return germanLabel;
+    }
+    public String getCssClass() {
+        switch (this) {
+            case SEEN:
+                return "seen-state";
+            case INTERESTED:
+                return "interested-state";
+            case NOT_INTERESTED:
+                return "not-interested-state";
+            case UNSURE:
+                return "unsure-state";
+            case MISS_OUT:
+                return "miss-out-state";
+            case NONE:
+            default:
+                return "none-state";
+        }
+    }
 }
