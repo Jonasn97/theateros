948cccf3df7ba6b4d6759aec0db7894bcc2aedc2
Jonas Nelsn <jonas.nelson@hs-osnabrueck.de>
2023-07-20 21:45:52 +0200

    feat: implemented PerformanceIdResourceApi

--- src/main/java/de/hsos/swa/jonas/theater/eventmanagement/boundary/resources/api/PerformanceIdResourceApi.java
+++ src/main/java/de/hsos/swa/jonas/theater/eventmanagement/boundary/resources/api/PerformanceIdResourceApi.java
@@ -1,4 +1,48 @@
 package de.hsos.swa.jonas.theater.eventmanagement.boundary.resources.api;
 
+import de.hsos.swa.jonas.theater.eventmanagement.boundary.dto.api.OutgoingPerformanceDTOApi;
+import de.hsos.swa.jonas.theater.eventmanagement.control.PerformanceOperations;
+import de.hsos.swa.jonas.theater.eventmanagement.entity.Performance;
+import de.hsos.swa.jonas.theater.shared.dto.jsonapi.ErrorDTO;
+import de.hsos.swa.jonas.theater.shared.dto.jsonapi.ResourceObjectDTO;
+import de.hsos.swa.jonas.theater.shared.dto.jsonapi.ResponseWrapperDTO;
+
+import javax.inject.Inject;
+import javax.validation.constraints.Positive;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.util.ArrayList;
+import java.util.Optional;
+@Path("api/performances/")
 public class PerformanceIdResourceApi {
+
+    @Inject
+    PerformanceOperations performanceOperations;
+
+    @Context
+    UriInfo uriInfo;
+
+    @Path("{performanceId}")
+    @GET
+    public Response getPerformanceById(@Positive @PathParam("performanceId") long eventId){
+        Optional<Performance> performance = performanceOperations.getPerformance(eventId);
+        ResponseWrapperDTO<Object> responseWrapperDTO = new ResponseWrapperDTO<>();
+        if(performance.isPresent()){
+            OutgoingPerformanceDTOApi outgoingEventIdDTOApi = OutgoingPerformanceDTOApi.Converter.toDTO(performance.get());
+            ResourceObjectDTO<OutgoingPerformanceDTOApi> resourceObjectDTO = new ResourceObjectDTO<>();
+            resourceObjectDTO.id = String.valueOf(performance.get().id);
+            resourceObjectDTO.type = "performance";
+            resourceObjectDTO.links = null; //TODO createSelfLink(resourceObjectDTO.id);
+            resourceObjectDTO.attributes = outgoingEventIdDTOApi;
+            responseWrapperDTO.data=resourceObjectDTO;
+            return Response.ok().entity(responseWrapperDTO).build();
+        }
+        responseWrapperDTO.errors = new ArrayList<>();
+        responseWrapperDTO.errors.add(new ErrorDTO("404", "PERF:5","Performance not found", "Couldn't find Performance with the given id"));
+        return Response.status(Response.Status.NOT_FOUND).entity(responseWrapperDTO).build();
+    }
 }
