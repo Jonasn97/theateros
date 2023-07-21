01b2ac9ee50113f8dcd84f37292d13a9ac5bd21e
Jonas Nelsn <jonas.nelson@hs-osnabrueck.de>
2023-07-21 23:25:09 +0200

    chore: Changed EnumType of PerformanceState

--- src/main/java/de/hsos/swa/jonas/theater/userdata/entity/UserPerformance.java
+++ src/main/java/de/hsos/swa/jonas/theater/userdata/entity/UserPerformance.java
@@ -3,9 +3,12 @@ package de.hsos.swa.jonas.theater.userdata.entity;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 
 @Entity
 public class UserPerformance extends PanacheEntity {
     public long performanceId;
+    @Enumerated(EnumType.STRING)
     public PerformanceState performanceState;
 }
